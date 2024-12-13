e3fea2ef2f1a6a090e0b889e67199eafbcc93acc
kris098e <abrahamsenkd@gmail.com>
2024-12-13 21:51:05 +0100

    day9 part1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -17,5 +17,6 @@ fun main() {
  //    day7.part1()
      //day7.part2()
     //day8.part1()
-    day8.part2()
+    //day8.part2()
+    day9.part1()
 }
--- src/main/kotlin/day9/Day9.kt
+++ src/main/kotlin/day9/Day9.kt
@@ -1,4 +1,41 @@
 package day9
 
+import org.example.util.ResourceLoader
 
+private val line = ResourceLoader.getResourceFile("/day9/day9.txt").readLines().first()
+
+fun part1() {
+    val fileBlocks = buildList { for (i in line.indices step 2) add(line[i].digitToInt()) }
+    val emptySpaces = buildList { for (i in 1 until line.lastIndex step 2) add(line[i].digitToInt()) }
+
+    val formattedInput = buildMap<Int, Int?> {
+        var indexCount = 0
+        for (i in fileBlocks.indices) {
+            for (j in 0 until fileBlocks[i]) {
+                put(indexCount, i)
+                indexCount++
+            }
+            if (i < emptySpaces.size) {
+                for (j in 0 until emptySpaces[i]) {
+                    put(indexCount, null)
+                    indexCount++
+                }
+            }
+        }
+    }.toMutableMap()
+
+    var indexOfFirstNull = formattedInput.values.indexOf(null)
+    for (i in formattedInput.values.size - 1 downTo 0) {
+        if (indexOfFirstNull > i) break
+        val tmp = formattedInput[i]
+        formattedInput[i] = null
+        formattedInput[indexOfFirstNull] = tmp
+        indexOfFirstNull = formattedInput.values.indexOf(null)
+    }
+
+    formattedInput.values.foldIndexed(0L) { index, acc, value ->
+        if (value == null) return@foldIndexed acc
+        acc +  value * index
+    }.let(::println)
+}
 
